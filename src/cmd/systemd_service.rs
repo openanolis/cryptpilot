2261aa24e811cdbd9352cbebd4f84be55e86c7f6
Kun Lai <laikun@linux.alibaba.com>
2024-10-31 11:21:25 +0800

    cmd/systemd-service: fix missing check for open_in_system option
    Signed-off-by: Kun Lai <laikun@linux.alibaba.com>

--- src/cmd/systemd_service.rs
+++ src/cmd/systemd_service.rs
@@ -4,12 +4,25 @@ use log::{error, info};
 use crate::cli::SystemdServiceOptions;
 
 pub async fn cmd_systemd_service(_systemd_service_options: &SystemdServiceOptions) -> Result<()> {
-    info!("Loading volume configs");
+    info!("Checking status for all volumes");
     let volume_configs = crate::config::load_volume_configs().await?;
+
+    if volume_configs.len() == 0 {
+        info!("The volume configs is empty, exit now");
+    }
+
     super::show::print_volume_configs_as_table(&volume_configs).await?;
 
     info!("Opening volumes according to volume configs");
     for volume_config in &volume_configs {
+        if volume_config.extra_options.open_in_system != Some(true) {
+            info!(
+                "The open_in_system option is not enabled for volume {}, skip now",
+                volume_config.volume
+            );
+            continue;
+        }
+
         info!(
             "Setting up mapping for volume {} from device {}",
             volume_config.volume, volume_config.dev
@@ -30,7 +43,7 @@ pub async fn cmd_systemd_service(_systemd_service_options: &SystemdServiceOption
         };
     }
 
-    info!("Checking status again");
+    info!("Checking status for all volumes again");
     super::show::print_volume_configs_as_table(&volume_configs).await?;
     info!("Everything have been completed, exit now");
 
