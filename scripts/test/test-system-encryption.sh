9b3ee03c171c8003957d8499d8a1033dcdb3f5d6
Kun Lai <laikun@linux.alibaba.com>
2025-09-04 17:43:38 +0800

    for test modprobe in github

--- scripts/test/test-system-encryption.sh
+++ scripts/test/test-system-encryption.sh
@@ -648,7 +648,8 @@ main() {
     encrypt_image
     
     # Calculate reference values
-    calculate_reference_values
+    # TODO: fix the udevadm problem by changing to guestmount https://github.com/openanolis/cryptpilot/actions/runs/17576971966/job/49925051838?pr=21
+    # calculate_reference_values
 
     # Handle CI mode
     if [[ "$CI_MODE" == true ]]; then
