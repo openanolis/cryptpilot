f7b2486d298959d455582de462687bb18388658f
panpingsheng <panpingsheng@hygon.cn>
2025-09-06 16:16:39 +0800

    cryptpilot-convert: Fixed the issue where the boot partition and efi partition could not be automatically mounted after the system started

--- cryptpilot-convert.sh
+++ cryptpilot-convert.sh
@@ -504,7 +504,7 @@ step:update_rootfs_and_initrd() {
 
     log::info "Updating /etc/fstab"
     # Prevent duplicate mounting of efi partitions
-    sed -i '/[[:space:]]\/boot\/efi[[:space:]]/ s/defaults,/defaults,noauto,nofail,/' "${rootfs_mount_point}/etc/fstab"
+    sed -i '/[[:space:]]\/boot\/efi[[:space:]]/ s/defaults,/defaults,auto,nofail,/' "${rootfs_mount_point}/etc/fstab"
 
     if [ "$boot_part_exist" = "false" ]; then
         # update /etc/fstab
@@ -517,7 +517,7 @@ step:update_rootfs_and_initrd() {
         ## insert boot mount line
         local boot_uuid
         boot_uuid=$(blkid -o value -s UUID "$boot_file_path") # get uuid of the boot image
-        local boot_mount_line="UUID=${boot_uuid} /boot ext4 defaults,noauto,nofail 0 2"
+        local boot_mount_line="UUID=${boot_uuid} /boot ext4 defaults,auto,nofail 0 2"
         local boot_mount_insert_line_number
         boot_mount_insert_line_number=$((root_mount_line_number + 1))
         sed -i "${boot_mount_insert_line_number}i${boot_mount_line}" "${rootfs_mount_point}/etc/fstab"
