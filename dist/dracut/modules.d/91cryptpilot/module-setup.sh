613f03277bb553a5db3dd01bf629b0a3a4df6ca5
Kun Lai <laikun@linux.alibaba.com>
2025-09-27 16:03:51 +0800

    boot_service: remove findmnt failed warm
    This fix the problem during boot which always report findmnt failed.
    
    Signed-off-by: Kun Lai <laikun@linux.alibaba.com>

--- dist/dracut/modules.d/91cryptpilot/module-setup.sh
+++ dist/dracut/modules.d/91cryptpilot/module-setup.sh
@@ -19,7 +19,7 @@ install() {
         inst_multiple vgchange lvcreate
         inst_multiple blkid lsblk findmnt
         inst_multiple dd tail grep sort
-        inst_multiple awk sed pvs growpart sfdisk lvm lvextend
+        inst_multiple awk sed pvs growpart sfdisk lvm lvextend mountpoint
         # required by 'file' command
         inst_multiple file
         inst_multiple modprobe
