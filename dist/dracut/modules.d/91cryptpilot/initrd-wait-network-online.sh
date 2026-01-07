b96c32021c8f221c91b56c9ef81491f5e849c7b1
panpingsheng <panpingsheng@hygon.cn>
2026-01-07 15:53:06 +0800

    dracut: improve cryptpilot initrd module
    - Add network online wait timeout (60 seconds)
    - Include bash in installed binaries
    
    Signed-off-by: panpingsheng <panpingsheng@hygon.cn>

--- dist/dracut/modules.d/91cryptpilot/initrd-wait-network-online.sh
+++ dist/dracut/modules.d/91cryptpilot/initrd-wait-network-online.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+TIMEOUT=60  # Timeout Time (seconds)
+COUNT=0
+
 while true; do
     if [ -e /var/run/cryptpilot/.network-online ]; then
+        echo "Network is online."
         break
     fi
     sleep 1
+    COUNT=$((COUNT + 1))
+
+    if [ $COUNT -ge $TIMEOUT ]; then
+        echo "Timeout after $TIMEOUT seconds waiting for network."
+        break
+    fi
 done
